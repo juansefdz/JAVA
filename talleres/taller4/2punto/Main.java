894e89d88085958de352b5c052fb65945c371987
J. Sebastián Fernández M <137450466+juansefdz@users.noreply.github.com>
2024-03-11 16:44:22 -0500

    Create Main.java

---
+++ talleres/taller4/2punto/Main.java
@@ -0,0 +1,13 @@
+
+public class Main {
+    public static void main(String[] args) {
+
+        Empleado objEmpleado = new Empleado("juan", "dev", 1000, 5);
+
+        System.out.println(objEmpleado);
+        objEmpleado.aumentarSalario();
+
+
+    }
+
+}
