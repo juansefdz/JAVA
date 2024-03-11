6b129909760f90fafd8fb5527ec663594035743a
J. Sebastián Fernández M <137450466+juansefdz@users.noreply.github.com>
2024-03-11 16:39:18 -0500

    Create Rectangulo.java

---
+++ talleres/taller4/4punto/Rectangulo.java
@@ -0,0 +1,14 @@
+public class Rectangulo  extends  Figura{
+   private double alto ;
+   private double ancho ;
+
+    public Rectangulo(double alto, double ancho) {
+        this.alto = alto;
+        this.ancho = ancho;
+    }
+
+    @Override
+    public double calcularArea(){
+        return this.ancho * this.alto;
+    }
+}
