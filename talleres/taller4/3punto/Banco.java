3b6f2cd7abba94aafe8ec1bac85d1458654385ba
J. Sebastián Fernández M <137450466+juansefdz@users.noreply.github.com>
2024-03-11 16:41:06 -0500

    Update Banco.java

--- talleres/taller4/3punto/Banco.java
+++ talleres/taller4/3punto/Banco.java
@@ -120,11 +120,4 @@ public class Banco {
 }
 
 
-public class Main {
-    public static void main(String[] args) {
-        Banco cuenta = new Banco("Juan", 1000.0);
-        cuenta.depositarDinero();
-        cuenta.retirarDinero();
-        System.out.println(cuenta);
-    }
-}
+
