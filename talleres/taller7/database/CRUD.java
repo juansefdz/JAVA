40d1a76d1b1352d92d29a32210c463d486e872c4
J. Sebastián Fernández M <137450466+juansefdz@users.noreply.github.com>
2024-03-19 19:32:03 -0500

    Add files via upload

--- talleres/taller7/database/CRUD.java
+++ talleres/taller7/database/CRUD.java
@@ -1,4 +1,27 @@
 package database;
 
+
 public interface CRUD {
+    //insert
+    public Object insert(Object object);
+
+    public boolean consult(Object object);
+
+    //consult
+    public Object findById(int id);
+
+    public Object findbyAuthor(Object object);
+
+    public Object findbytitle(Object object);
+
+    //showall
+    public List<Object> findAll();
+
+    //update
+    public boolean update(Object object);
+
+    //delete
+    public boolean delete(Object object);
+
+
 }
